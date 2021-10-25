dab736fea88db7ad5e1fdb9c60c557974c9efa68
HanSeung_Lee <gkstmd1014@naver.com>
2021-10-25 11:16:23 +0900

    no message

--- algorithmMid.cpp
+++ algorithmMid.cpp
@@ -24,28 +24,19 @@ void CalPath(int P[][n + 1])
 	}
 }
 
-int CalP(int i, int j)
-{
-	if (C[i][j] == 0)
-		return 0;
-	else if (j == 1 || i == 1)
-		return 1;
-	else
-		return CalP(i - 1, j) + CalP(i, j - 1);
-}
-
 int main()
 {
 	int P[m + 1][n + 1] = { 0, };
 	memset(C, 1, sizeof(C));
 	C[1][5] = 0; C[3][3] = 0; C[5][2] = 0;
-	cout << CalP(6, 5);
-	/*for (int i = 1; i <= m; i++)
+	CalPath(P);
+
+	for (int i = 1; i <= m; i++)
 	{
 		for (int j = 1; j <= n; j++)
 			cout << P[i][j] << "\t";
 		cout << endl;
-	}*/
+	}
 
 	return 0;
 }
